68e7cd75c0a5647a74bb0146e8dc97f01e89a3ad
Julie Reynal <rjulie.pro@gmail.com>
2024-05-17 22:56:32 +0200

    App: update component with new lib

--- components/Cursor/index.js
+++ components/Cursor/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import AnimatedCursor from "react-animated-cursor";
 // import CustomCursor from "custom-cursor-react";
 // import "custom-cursor-react/dist/index.css";
+
 import { useTheme } from "next-themes";
 
 const Cursor = () => {
@@ -21,19 +23,43 @@ const Cursor = () => {
   return (
     <>
       {mount && (
-        <CustomCursor
-          targets={[".link"]}
-          customClass="custom-cursor"
-          dimensions={30}
-          fill={getCusomColor()}
-          smoothness={{
-            movement: 0.2,
-            scale: 0.1,
-            opacity: 0.2,
-          }}
-          targetOpacity={0.5}
-          targetScale={2}
+        // <CustomCursor
+        //   targets={[".link"]}
+        //   customClass="custom-cursor"
+        //   dimensions={30}
+        //   fill={getCusomColor()}
+        //   smoothness={{
+        //     movement: 0.2,
+        //     scale: 0.1,
+        //     opacity: 0.2,
+        //   }}
+        //   targetOpacity={0.5}
+        //   targetScale={2}
+        // />
+
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={8}
+          color={getCusomColor()}
+          outerAlpha={0.2}
+          innerScale={0.7}
+          outerScale={5}
+          clickables={[
+            'a',
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            'label[for]',
+            'select',
+            'textarea',
+            'button',
+            '.link'
+          ]}
         />
+
+
       )}
     </>
   );
