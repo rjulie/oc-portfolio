df28e76796262f297966ab05411e3cf6a181cbfa
Julie Reynal <rjulie.pro@gmail.com>
2024-05-23 23:20:49 +0200

    Tag: add css

--- components/Tags/index.js
+++ components/Tags/index.js
@@ -1,12 +1,11 @@
-import PropTypes from 'prop-types'
-
 function Tags({ tags }) {
+
   return (
     <div>
       {tags ? (
         <div className="flex flex-row gap-2">
           {tags.map((tag, index) => (
-            <div className="tag" key={index}>
+            <div className={`tag border border-gray-500 rounded px-1.5 py-0.5 text-sm text-gray-500`} key={index}>
               {tag}
             </div>
           ))}
@@ -16,8 +15,5 @@ function Tags({ tags }) {
   )
 }
 
-Tags.propTypes = {
-  tags: PropTypes.array,
-}
 
 export default Tags
