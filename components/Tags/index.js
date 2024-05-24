0cd0b2d5891804e25f2e8a9cbc0cb8367ecf6420
Julie Reynal <rjulie.pro@gmail.com>
2024-05-24 17:43:29 +0200

    Tag: refacto add flex-wrap

--- components/Tags/index.js
+++ components/Tags/index.js
@@ -3,7 +3,7 @@ function Tags({ tags }) {
   return (
     <div>
       {tags ? (
-        <div className="flex flex-row gap-2">
+        <div className="flex flex-row flex-wrap gap-2">
           {tags.map((tag, index) => (
             <div className={`tag border border-gray-500 rounded px-1.5 py-0.5 text-sm text-gray-500`} key={index}>
               {tag}
